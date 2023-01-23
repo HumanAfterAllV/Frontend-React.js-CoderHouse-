61bde39417efae27f924e8126852d85755d96d9e
Haav <motocycle9719@hotmail.com>
2023-01-23 17:42:57 -0600

    Modificando aspecto

--- src/assets/img/Img.js
+++ src/assets/img/Img.js
@@ -1,11 +1,9 @@
 import shoppingBag from "./shoppingBag.png";
 import menu from "./menu.png";
-import person from "./noun-person-2421164.png";
 
 
 
 export default [
     shoppingBag,
     menu,
-    person
 ]
--- src/components/CardWidget.js
+++ src/components/CardWidget.js
@@ -1,11 +1,9 @@
-import Img from "../assets/img/Img";
 import ItemListContainer from "./ItemListContainer";
 
 const CardWidget = () =>
 {
     return(
         <div className="header__Nav--Icon animate__animated animate__slideInDown" >
-            <a href=""><img className="header__Nav--Icon__element--person" src={Img[2]} alt="" /></a>
             <ItemListContainer/>
         </div>
     );
