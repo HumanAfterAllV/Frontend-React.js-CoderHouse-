d1c47d2c343cbf9f8f544f9bd4f0e8d6540fa1f5
Haav <motocycle9719@hotmail.com>
2023-01-31 14:26:30 -0600

    Retrospectiva color

--- src/assets/img/Img.js
+++
@@ -1,9 +0,0 @@
-import shoppingBag from "./icons/shoppingBag.png";
-import menu from "./icons/menu.png";
-
-
-
-export default [
-    shoppingBag,
-    menu,
-]
--- src/components/Side.js
+++ src/components/Side.js
@@ -3,7 +3,12 @@ const Side = () => {
     <>
     <aside id="aside">
       <div className="aside__chl">
-
+        <ul className="aside__chl--ul">
+          <li className="aside__chl--ul__item"><a href="">Home</a></li>
+          <li className="aside__chl--ul__item"><a href="">Shop</a></li>
+          <li className="aside__chl--ul__item"><a href="">Blog</a></li>
+          <li className="aside__chl--ul__item"><a href="">Contact</a></li>
+        </ul>
       </div>
     </aside>
     </>
